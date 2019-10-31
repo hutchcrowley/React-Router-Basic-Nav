97cebf7549b8ceb84c8e8ceaddf4b337f82307d0
Hutch Crowley <tothecoda@gmail.com>
2019-10-31 15:18:37 -0500

    init commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Navigation from './Navigation';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Navigation from './Navigation'
 
-export { Home, About, Contact, Navigation };
+export { Home, About, Contact, Navigation }
